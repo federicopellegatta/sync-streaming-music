e9e9cb697dc709d9bf86edb40f7aa67f63b7704b
Federico Pellegatta <pellegatta.federico@gmail.com>
2023-03-24 18:45:34 +0100

    add menu to manage case no playlist selected

--- src/cli/menu.py
+++ src/cli/menu.py
@@ -34,5 +34,19 @@ def playlists_checkbox(playlists: list[Playlist]) -> list[Playlist]:
 
     if answers["isAll"]:
         return playlists
+
+    elif answers["playlists"] == []:
+        questions = [
+            inquirer.Confirm("isEmpty",
+                             message=f"{bcolors.BOLD}You have not selected any playlists. Do you want to exit?{bcolors.ENDC}",
+                             default=False),
+        ]
+
+        if (inquirer.prompt(questions)["isEmpty"]):
+            exit(0)
+        else:
+            print()
+            return playlists_checkbox(playlists)
+
     else:
         return list(playlist for playlist in playlists if playlist.name.casefold() in [playlist.casefold() for playlist in answers["playlists"]])
